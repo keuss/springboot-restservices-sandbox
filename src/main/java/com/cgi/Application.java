7102205d79d34bff565149f5854595d698db0730
guillaume gallois <guillaume.gallois@gmail.com>
2017-04-27 14:30:31 +0200

    EnableSwagger2

--- src/main/java/com/cgi/Application.java
+++ src/main/java/com/cgi/Application.java
@@ -2,11 +2,42 @@ package com.cgi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    // see http://heidloff.net/article/usage-of-swagger-2-0-in-spring-boot-applications-to-document-apis/
+    // for Swagger test
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("users")
+                .select()
+                .paths(regex("/user.*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .version("0.1.0")
+                .contact("keuss")
+                .build();
     }
 }
