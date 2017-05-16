6d2b732ba1357f4aeb123e60703aec8e38e632d3
x153216 Guillaume GALLOIS <guillaume.gallois-ext@socgen.com>
2017-05-16 16:25:08 +0200

    add test.properties for UserControllerTU

--- src/test/java/com/cgi/restcontroller/UserControllerTU.java
+++ src/test/java/com/cgi/restcontroller/UserControllerTU.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = com.cgi.Application.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestPropertySource(locations="classpath:test.properties")
 @AutoConfigureMockMvc
 public class UserControllerTU {
 
