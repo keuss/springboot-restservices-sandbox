365702e881e94e5af654327c95ca8ba01f3fdeea
keuss <guillaume.gallois@gmail.com>
2017-09-08 16:13:42 +0200

    add api path add clean swagger

--- src/main/java/com/cgi/Application.java
+++ src/main/java/com/cgi/Application.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -25,9 +26,9 @@ public class Application {
     @Bean
     public Docket newsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("users")
                 .select()
-                .paths(regex("/users.*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/users.*"))
                 .build()
                 .apiInfo(apiInfo());
     }
@@ -37,7 +38,7 @@ public class Application {
                 .title("Spring REST Sample with Swagger")
                 .description("Spring REST Sample with Swagger")
                 .version("0.1.0")
-                .contact("keuss")
+                .contact(new Contact("keuss", "https://github.com/keuss", "guillaume.gallois@gmail.com"))
                 .build();
     }
 }
