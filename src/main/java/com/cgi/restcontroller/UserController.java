97aa45871287ebd77f8acc24b96a7de1c43c0f91
keuss <guillaume.gallois@gmail.com>
2017-09-08 16:53:19 +0200

    use of ResponseEntity and Custom Exception

--- src/main/java/com/cgi/restcontroller/UserController.java
+++ src/main/java/com/cgi/restcontroller/UserController.java
@@ -2,10 +2,13 @@ package com.cgi.restcontroller;
 
 import com.cgi.entities.User;
 import com.cgi.services.UserService;
+import com.cgi.utils.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,11 +37,11 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/users", headers = {"Content-type=application/json"}, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public User addUser(@Valid @RequestBody User user, BindingResult bindingResults) throws Exception {
-        if (bindingResults.hasErrors()) {
-            throw new Exception("Input invalid");
+    public ResponseEntity<?> addUser(@Valid @RequestBody User user, BindingResult bindingResults) {
+        if (bindingResults.hasErrors() || user == null || user.getName() == null || user.getName().isEmpty()) {
+            throw new BadRequestException();
         } else {
-            return this.userService.create(user);
+            return new ResponseEntity<>(this.userService.create(user), HttpStatus.CREATED);
         }
     }
 }
