80622e9c809c2eb8b6513035e44e6688a210b73d
Guillaume GALLOIS <guillaume.gallois@gmail.com>
2017-10-20 11:03:52 +0200

    add static content for test with jquery

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -3,9 +3,44 @@ import ReactDOM from 'react-dom'
 
 const MOUNT_NODE = document.getElementById('root')
 
+
+var UserTab = (props) => {
+  return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Height</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props.name}</td>
+            <td>27</td>
+            <td>1,91</td>
+            <td>Akron, OH</td>
+          </tr>
+          <tr>
+            <td>Klay Thompson</td>
+            <td>25</td>
+            <td>2,01</td>
+            <td>Los Angeles, CA</td>
+          </tr>
+        </tbody>
+      </table>
+  )
+}
+
 // A functional component using an ES2015 (ES6) arrow function
 var Welcome = (props) => {
-  return <h2>Hello, world from {props.name} !</h2>
+  return (
+          <div>
+            <h2>Hello, world from {props.name} !</h2>
+            <UserTab name={props.name} />
+          </div>  
+         )
 }
 
 const element = <Welcome name="Keuss" />
