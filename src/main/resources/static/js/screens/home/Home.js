c00bc54a5752912264b1cc402859592e4737a49f
Guillaume GALLOIS <guillaume.gallois@gmail.com>
2017-10-25 18:18:31 +0200

    add screens and components

--- src/main/resources/static/js/screens/home/Home.js
+++ src/main/resources/static/js/screens/home/Home.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import UserInfo from '../../components/UserInfo'
+import UserPost from '../../components/UserPost'
+
 
 export default class Home extends React.Component {
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="column column-10">
-                        <img className="round" src={"assets/images/img_avatar.png"} />
-                    </div>
-                    <div className="column column-90">
-                        <blockquote>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</blockquote>
-                    </div>
+                    <UserInfo user={{ name: 'Marc M.', avatar: 'assets/images/img_avatar.png'}}/>
+                    <UserPost />
                 </div>
                 <div className="row">
-                    <div className="column column-10">
-                        <img className="round" src={"assets/images/img_avatar2.png"} />
-                    </div>
-                    <div className="column column-90">
-                        <blockquote>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</blockquote>
-                    </div>
+                    <UserInfo user={{ name: 'Steve D.', avatar: 'assets/images/img_avatar2.png' }} />
+                    <UserPost />
+                </div>
+                <div className="row">
+                    <UserInfo user={{ name: 'Lucy S.', avatar: 'assets/images/img_avatar2.png' }} />
+                    <UserPost />
+                </div>
+                <div className="row">
+                    <UserInfo user={{ name: 'Dave G.', avatar: 'assets/images/img_avatar.png' }} />
+                    <UserPost />
                 </div>
             </div>
         )
